74437bc819ee85802a0f67c8989b06c9dd9745f6
Emmanuel Franco <emmanuelfranco2001@gmail.com>
2024-04-29 16:35:54 -0300

    operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,29 @@
+
+public class Operadores {
+
+    public static void main(String[] args) {
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+        
+        concatenacao = 1+"1"+1+"1";
+        
+        System.out.println(concatenacao);
+        
+        concatenacao = "1"+1+1+1;
+        
+        System.out.println(concatenacao);
+        
+        concatenacao = (1+1+1)+"1";
+        
+        System.out.println(concatenacao);
+
+        int numero = 5;
+        System.out.println(-numero);
+        System.out.println(numero);
+        System.out.println(++numero);
+        System.out.println(--numero);
+    }
+}
