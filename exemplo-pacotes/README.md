c9cdfb582813e203e7aad0cd3058af6fbd923a66
Emmanuel Franco <emmanuelfranco2001@gmail.com>
2024-05-10 13:21:35 -0300

    pacotes

---
+++ exemplo-pacotes/README.md
@@ -0,0 +1,5 @@
+## Default, public, private
+
+ - Métodos Default só podem ser acessados pelas classes que estão no mesmo pacote/package
+ - Métodos Public podem ser acessados por qualquer classe
+ - Métodos Private só podem ser acessados dentro da classe na qual o método está localizado
---
+++ exemplo-pacotes/src/lanchonete/atendimento/cozinha/Almoxarife.java
@@ -0,0 +1,17 @@
+package lanchonete.atendimento.cozinha;
+
+public class Almoxarife {
+    private void controlarEntrada() {
+		System.out.println("CONTROLANDO A ENTRADA DOS ITENS");
+	}
+	private void controlarSaida() {
+		System.out.println("CONTROLANDO A SAIDA DOS ITENS");
+	}
+	void entregarIngredientes() {//forma default
+		System.out.println("ENTREGANDO INGREDIENTES");
+		//...?
+	}
+	void trocarGas() {//forma default
+		System.out.println("ALMOXARIFE TROCANDO O GÁS");
+	}
+}
